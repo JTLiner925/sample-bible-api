cd6d44509d6688bcc5109541d97fe2010cc2aee7
JTLiner925 <[jtliner925@gmail.com]>
2020-03-13 10:02:54 -0500

    working through the sample demo on bible api site, just finished list of books section

--- index.js
+++ index.js
@@ -1,60 +1,80 @@
-const apiKey = 'bf4ad93afca94d0a8c4df0261f6de6cb';
-const searchURL = 'https://newsapi.org/v2/everything';
 
-function formatQueryParams(params){
-  const queryItems = Object.keys(params)
-    .map(key => `${key}=${params[key]}`);
-  return queryItems.join('&');
-}
 
-function displayResults(responseJson, maxResults){
-  $('#results-list').empty();
-  for(let i = 0; i< responseJson.articles.length & i<maxResults; i++){
-    $('#results-list').append(
-      `<li><h3><a href="${responseJson.articles[i].url}">${responseJson.articles[i].title}</a></h3>
-      <p>${responseJson.articles[i].source.name}</p>
-      <p>By ${responseJson.articles[i].author}</p>
-      <p>${responseJson.articles[i].description}</p>
-      <img src='${responseJson.articles[i].urlToImage}'>
-      </li>`
-    );}
-  $('#results').removeClass('hidden');
-}
-function getNews(query, maxResults=10) {
-  const params = {
-    q: query,
-    language: 'en',
-  };
-  const queryString = formatQueryParams(params);
-  const url = searchURL + '?' + queryString;
-  console.log(url);
+const versionList = document.querySelector(`#bible-version-list`);
+let versionHTML = ``;
+getBibleVersions().then((bibleVersionList) => {
+  const sortedVersions = sortVersionsByLanguage(bibleVersionList);
 
-  const options = {
-    headers: new Headers({
-      "X-Api-Key": apiKey})
-  };
+  for (let languageGroup in sortedVersions) {
+    const language = languageGroup;
+    versionHTML += `<h4 class="list-heading"><span>${language}</span></h4><ul>`;
+    const versions = sortedVersions[languageGroup];
+    for (let version of versions) {
+      versionHTML += `<li class="bible">
+                        <a href="book.html?version=${version.id}&abbr=${version.abbreviation}">
+                          <abbr class="bible-version-abbr" title="${version.name}">${version.abbreviation}</abbr>
+                          <span>
+                            <span class="bible-version-name">${version.name}</span>
+                            ${version.description ? '<span class="bible-version-desc">' + version.description + "</span>" : ''}
+                          </span>
+                        </a>
+                      </li>`;
+    }
+    versionHTML += `</ul>`;
+  }
+  versionList.innerHTML = versionHTML;
+});
+function getBibleVersions() {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
 
-  fetch(url, options)
-    .then(response => { 
-      if(response.ok){
-        return response.json();
+    xhr.addEventListener(`readystatechange`, function() {
+      if (this.readyState === this.DONE) {
+        const {data} = JSON.parse(this.responseText);
+        const versions = data.map( (data) => {
+          return {
+            name: data.name,
+            id: data.id,
+            abbreviation: data.abbreviation,
+            description: data.description,
+            language: data.language.name
+          };
+        });
+
+        resolve(versions);
       }
-      throw new Error(response.statusText);
-    })
-    .then(responseJson => displayResults(responseJson, maxResults))
-    .catch(err => { $('#js-error-message')
-      .text(`Something went wrong: ${err.message}`);
     });
-    
-}
 
-function watchForm(){
-  $('form').submit(event => {
-    event.preventDefault();
-    const searchTerm = $('#js-search-term').val();
-    const maxResults = $('#js-max-results').val();
-    getNews(searchTerm, maxResults);
+    xhr.open(`GET`, `https://api.scripture.api.bible/v1/bibles`);
+    xhr.setRequestHeader(`api-key`, API_KEY);
+
+    xhr.onerror = () => reject(xhr.statusText);
+
+    xhr.send();
   });
 }
+function sortVersionsByLanguage(bibleVersionList) {
+  let sortedVersions = {};
 
-$(watchForm);
+  for (const version of bibleVersionList) {
+    if (!sortedVersions[version.language]) {
+      sortedVersions[version.language] = [];
+    }
+    sortedVersions[version.language].push(version);
+  }
+  for (const version in sortedVersions) {
+    sortedVersions[version].sort( (a, b) => {
+      const nameA = a.abbreviation.toUpperCase();
+      const nameB = b.abbreviation.toUpperCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    } );
+  }
+  return sortedVersions;
+}
---
+++ my_key.js
@@ -0,0 +1,2 @@
+const API_KEY = `8a05313f71a01b1a25ce44f5d1aa179a`;
+//need to make this only available on server side
